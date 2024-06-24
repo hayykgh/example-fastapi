a79781f4254061be4e6432f82a6a9f913fcdf34d
hayykgh <hayykgh@gmail.com>
2024-06-24 17:28:01 +0900

    added the changes tied to the FE for deploying to prod

--- ferny/vue.config.js
+++ ferny/vue.config.js
@@ -1,16 +1,5 @@
-// vue.config.js
-const webpack = require('webpack');
-
 module.exports = {
-  // Other configuration options...
-
-  configureWebpack: {
-    plugins: [
-      new webpack.DefinePlugin({
-        __VUE_PROD_DEVTOOLS__: JSON.stringify(false),
-        __VUE_OPTIONS_API__: JSON.stringify(true),
-        // Add other flags as needed
-      })
-    ]
-  }
+  outputDir: 'dist',
+  assetsDir: 'assets',
+  // Add more configuration options as needed
 };
