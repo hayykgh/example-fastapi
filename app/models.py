5d6b974368367421f0b559daab3f3b0c6628aa43
hayykgh <hayykgh@gmail.com>
2024-06-27 23:23:59 +0900

    comment email

--- app/models.py
+++ app/models.py
@@ -35,6 +35,3 @@ class Vote(Base):
     
     user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True)
     post_id = Column(Integer, ForeignKey("posts.id", ondelete="CASCADE"), primary_key=True)
-
-
-
